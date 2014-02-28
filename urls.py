f206f4a4a1e9fc79714e93689b08fad8d58ae082
Esra Altintas <es.altintas@gmail.com>
2014-02-28 13:17:32 +0200

    Added rest api, related #14

--- urls.py
+++ urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
-from api import SongResource
-
+from api import SongResource, FileResource, AlbumResource, SingerResource
+album_resource = AlbumResource()
+singer_resource = SingerResource()
+file_resource = FileResource()
 song_resource = SongResource()
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -15,6 +17,9 @@ urlpatterns = patterns('',
    # (r'^login/$', 'web.views.login_user'),
     (r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
     (r'^gramafon/$', 'web.views.gramafon'),
-    (r'^api/', include(song_resource.urls))	#Added song_resource for rest-api
+#    (r'^api/', include(song_resource.urls))	#Added song_resource for rest-api
+#    (r'^api/', include(file_resource.urls))    #Added file_resource for rest_api
+#    (r'^api/', include(singer_resource.urls))  #Added singer_resource for rest_api
+    (r'^api/', include(album_resource.urls))    #Added album_resource for rest_api
 )
 

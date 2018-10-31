8267d5d2799fbcb0b70b366129ea5216db056947
Burcin Akalın <brcnakalin@gmail.com>
2018-10-31 22:48:27 +0300

    Şarkılar çalınabilir duruma geldi.

--- app/app.py
+++ app/app.py
@@ -2,7 +2,10 @@ import eyed3
 from flask import Flask, render_template, request
 import pygame
 import os
-from flask import request
+import json
+from flask import jsonify
+
+# from flask.ext.triangle import Triangle
 
 app = Flask(__name__)
 
@@ -11,34 +14,37 @@ music = "/home/burcin/song/01.TheCrushOfLove.mp3"
 pygame.init()
 
 pygame.mixer.music.load(music)
-song_info_list = []
+
 
 
 @app.route("/")
 def song_information():
+    song_info_list = []
     songs = os.listdir(songs_path)
     for song in songs:
         if song.endswith(".mp3"):
             path = songs_path + song
             audiofile = eyed3.load(path)
             song_info = {'artist': audiofile.tag.artist, 'album': audiofile.tag.artist, 'title': audiofile.tag.title,
-                         'genre': audiofile.tag.genre, 'path': path}
+                         'path': path}
             song_info_list.append(song_info)
-    return render_template('index.html', song_info_list = song_info_list)
+    return render_template("index.html", song_info_list=song_info_list)
 
 
-@app.route("/play", methods=['GET','POST'])
+@app.route("/play", methods=['POST'])
 def play():
-    # path1 = request.form['path']
-    # print(path1)
-    #import pdb
-    #pdb.set_trace()
-    #music1 = "/home/burcin/song" + title
-    #print(music1)
-    # pygame.mixer.music.load(path1)
+    # import ipdb
+    # ipdb.set_trace()
+
+    request_body = json.loads(request.get_data())
+
+
+    song_path=request_body['song']
+
+    pygame.mixer.music.load(song_path)
     pygame.init()
     pygame.mixer.music.play()
-    return 'basarili'
+    return "basarili"
 
 
 @app.route("/stop", methods=['POST'])
@@ -48,4 +54,4 @@ def stop():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
